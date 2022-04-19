fb10610e4a73640e5cb1e1204b8e960babc9455a
Richard Wheatley <dickyw71@users.noreply.github.com>
2022-04-19 18:04:37 +0100

    Added content security policy directives for default source and script source.

--- myApp.js
+++ myApp.js
@@ -25,6 +25,13 @@ app.use(helmet.dnsPrefetchControl({
 
 app.use(helmet.noCache())
 
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "trusted-cdn.com"]
+  }
+}))
+
 
 
 
